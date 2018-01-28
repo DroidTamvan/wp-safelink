9d195b6f1b78b53b090ef3437bf30a1b841f5ddb
Masga Satria Wirawan <34214676+nawariwairtasagsam@users.noreply.github.com>
2018-01-28 10:11:56 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -6,17 +6,15 @@ if(window._wp_safelink_location){
 	}else{
 		window._wp_safelink_except = [window._wp_safelink_location, location.host];
 	}
-	window.parseUrl = function(string){
-		return /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/.exec(string);
-	}
 	window._wp_safelink = function(){
 		var anchors = document.querySelectorAll('a[href]');
 		anchors.forEach(function(anchor){
 			var url = 'http://' + window._wp_safelink_location + '/safelink/api/?data=';
-			var parse = window.parseUrl(anchor.href);
-			var origin = parse[3];
 			var check = false;
-			if(origin && window._wp_safelink_except.indexOf(origin) === -1){
+			window._wp_safelink_except.forEach(function(except){
+				check = anchor.href.search(except) > -1;
+			});
+			if(origin && !check){
 				anchor.href = url + btoa(encodeURIComponent(anchor.href)) + ".ref";
 			}
 		});
